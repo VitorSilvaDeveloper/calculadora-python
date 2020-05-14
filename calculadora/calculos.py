501537eb1a0ff51c8ca76f83e6622daf20fd6eed
VitorSorriso <vitorbackinha@gmail.com>
2020-05-14 18:00:03 -0300

    arquivo com as funções de calculos

---
+++ calculadora/calculos.py
@@ -0,0 +1,13 @@
+def soma(n1, n2):
+    return n1 + n2
+
+
+def subtracao(n1,n2):
+    return n1 - n2
+
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+def divisao(n1, n2):
+    return n1 / n2
+    
